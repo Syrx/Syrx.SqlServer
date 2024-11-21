911ec8554711e9d1ccba4427d67eca5823286752
David Sexton <sextondjc@users.noreply.github.com>
2024-11-21 17:11:15 +0000

    Split from 2.1.0
    This is split from the 2.1.0 version on the Syrx core repository. This is to allow for easier maintenance and package management.

---
+++ tests/integration/Syrx.SqlServer.Tests.Integration/SqlServerInstaller.cs
@@ -0,0 +1,33 @@
+﻿namespace Syrx.SqlServer.Tests.Integration
+{
+    public class SqlServerInstaller 
+    {
+        public SyrxBuilder SyrxBuilder { get; }
+        public IServiceProvider Provider { get; }
+
+        public SqlServerInstaller(string connectionString)
+        {
+            var services = new ServiceCollection();
+            var builder = new SyrxBuilder(services);
+            SyrxBuilder = builder.SetupSqlServer(connectionString);
+                        
+            Provider = services.BuildServiceProvider();
+            var commander = Provider.GetService<ICommander<DatabaseBuilder>>();
+            var database = new DatabaseBuilder(commander);
+            database.Build();
+        }
+    }
+
+    public static class SyrxInstaller
+    {
+        public static IServiceCollection Install(this IServiceCollection services)
+        {
+            return services.UseSyrx(factory =>
+                factory.UseSqlServer(builder =>
+                    builder
+                        .AddConnectionStrings()
+                        .AddSetupBuilderOptions()
+                ));
+        }
+    }
+}
