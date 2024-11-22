b48a5527831c6154aaeebbe9f8494e35b9dd619d
David Sexton <sextondjc@users.noreply.github.com>
2024-11-22 18:46:55 +0000

    Updates for 2.2.0
    Minor updates for floating versions.

--- tests/integration/Syrx.SqlServer.Tests.Integration/SqlServerInstaller.cs
+++ tests/integration/Syrx.SqlServer.Tests.Integration/SqlServerInstaller.cs
@@ -2,14 +2,13 @@
 {
     public class SqlServerInstaller 
     {
-        public SyrxBuilder SyrxBuilder { get; }
         public IServiceProvider Provider { get; }
 
         public SqlServerInstaller(string connectionString)
         {
             var services = new ServiceCollection();
             var builder = new SyrxBuilder(services);
-            SyrxBuilder = builder.SetupSqlServer(connectionString);
+            _ = builder.SetupSqlServer(connectionString);
                         
             Provider = services.BuildServiceProvider();
             var commander = Provider.GetService<ICommander<DatabaseBuilder>>();
@@ -18,16 +17,4 @@
         }
     }
 
-    public static class SyrxInstaller
-    {
-        public static IServiceCollection Install(this IServiceCollection services)
-        {
-            return services.UseSyrx(factory =>
-                factory.UseSqlServer(builder =>
-                    builder
-                        .AddConnectionStrings()
-                        .AddSetupBuilderOptions()
-                ));
-        }
-    }
 }
